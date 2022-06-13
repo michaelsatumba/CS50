0150ed598160b04d2664ff6a6f0fd0d06103bf74
michaelsatumba <mjsatumba@gmail.com>
2022-06-13 11:25:10 -0700

    Create hi.c

---
+++ week2-arrays/hi/hi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // char c1 = 'H';
+    // char c2 = 'I';
+    // char c3 = '!';
+
+    // printf("%i %i %i\n", c1, c2, c3);
+
+    string s = "HI!";
+    printf("%s\n", s);
+    printf("%i %i %i %i %i\n", s[0], s[1], s[2], s[3], s[4]);
+
+}
