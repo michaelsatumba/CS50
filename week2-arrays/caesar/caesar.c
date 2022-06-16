d471071426435514bda4d536a3469d734b0e7b28
michaelsatumba <mjsatumba@gmail.com>
2022-06-16 15:48:46 -0700

    Update caesar.c

--- week2-arrays/caesar/caesar.c
+++ week2-arrays/caesar/caesar.c
@@ -10,17 +10,21 @@ char rotate(char c, int n);
 int main(int argc, string argv[])
 {
     char ciphertext;
+
     if (argc < 2 || argc > 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
-    } else
+    }
+    else
     {
         if(only_digits(argv[1]) == false)
     {
          printf("Usage: ./caesar key\n");
         return 1;
-    } else {
+    }
+    else
+    {
         // printf("true\n");
         // printf("%s\n", argv[1]);
         // printf("%i\n", atoi(argv[1]));
@@ -42,11 +46,25 @@ bool only_digits(string s)
 {
     int length = strlen(s);
     // printf("%i\n", length);
-    if (isdigit(s[0])){
-       return true;
+    int truey = 0;
+    int falsy = 0;
+    for (int i = 0; i < length; i++){
+    // printf("%c\n", s[i]);
+    if(isdigit(s[i])){
+        // printf("true\n");
+        truey++;
     } else {
-        return false;
+        // printf("false\n");
+        falsy++;
     }
+    }
+    if (falsy > 0){
+       return false;
+    } else {
+        return true;
+    }
+
+
     // return false;
     // return true;
 }
@@ -56,16 +74,33 @@ char rotate(char c, int n)
     // printf("%i", (int)c);
     // printf(" rotate %i", (int)n);
     int ciphertext;
-    if(isalpha(c)){
-        // printf("%i", (int)c);
-        c = (int)c - 97;
-        // printf("%i", (int)c);
-        ciphertext = c + n;
-        ciphertext = ciphertext + 97;
-    } else {
+    if(isalpha(c))
+    {
+        if(isupper(c))
+        {
+            // printf("%i", (int)c);
+            c = (int)c - 65;
+            // printf("%i", (int)c);
+            ciphertext = (c + n) % 26;
+            // printf("%i", (int)ciphertext);
+            ciphertext = ciphertext + 65;
+            // printf("%i", (int)ciphertext);
+        }
+        else
+        {
+            // printf("%i", (int)c);
+            c = (int)c - 97;
+            // printf("%i", (int)c);
+            ciphertext = (c + n) % 26;
+            // printf("%i", (int)ciphertext);
+            ciphertext = ciphertext + 97;
+            // printf("%i", (int)ciphertext);
+        }
+    }
+    else
+    {
         ciphertext = c;
     }
-
     // printf("%i", first);
     // printf("%c", (char)first);
     return ciphertext;
